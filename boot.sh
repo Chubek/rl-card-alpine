16143e3f7d53895d373e876b27c6b6eed509c3ff
Chubak <chubakbiddpaa@gmail.com>
2022-03-13 07:46:45 +0330

    python2

--- boot.sh
+++ boot.sh
@@ -1,10 +1,10 @@
 #1/bin/sh
 
-cd rlcard/server && python3.8 manage.py runserver > /home/logs/server.log
+cd rlcard/server && python3.9 manage.py runserver > /home/logs/server.log
 
-cd /rlcard/pve_server && python3.8  run_douzero.py > /home/logs/douzero.log
+cd /rlcard/pve_server && python3.9  run_douzero.py > /home/logs/douzero.log
 
-cd /rlcard/pve_server && python3.8  python3 run_dmc.py > /home/logs/dmc.log
+cd /rlcard/pve_server && python3.9  python3 run_dmc.py > /home/logs/dmc.log
 
 
 cd rlcard && npm start
