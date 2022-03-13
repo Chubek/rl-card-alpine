c48f939e4982b59a164a180aff59fb2e6a2c04ac
Chubak <chubakbiddpaa@gmail.com>
2022-03-13 08:43:47 +0330

    problem fixed

--- boot.sh
+++ boot.sh
@@ -2,9 +2,9 @@
 
 cd rlcard/server && python3.9 manage.py runserver > /home/logs/server.log
 
-cd /rlcard/pve_server && python3.9  run_douzero.py > /home/logs/douzero.log
+cd rlcard/pve_server && python3.9  run_douzero.py > /home/logs/douzero.log
 
-cd /rlcard/pve_server && python3.9  python3 run_dmc.py > /home/logs/dmc.log
+cd rlcard/pve_server && python3.9  python3 run_dmc.py > /home/logs/dmc.log
 
 
 cd rlcard && npm start
